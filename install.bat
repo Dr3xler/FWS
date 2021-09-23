16b594935527ce666236eb0af233613dc98a2b76
Dr3xler <14806947+Dr3xler@users.noreply.github.com>
2021-09-23 16:58:31 +0200

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,8 @@
+cd "C:/FWS/Downloads/"
+echo "choose what you want to install"
+echo "1: Firefox"
+echo "2: Sync"
+echo "3: BitDefender"
+echo "4: all"
+
+Rem input check what number with switch case, then install with start file.exe
