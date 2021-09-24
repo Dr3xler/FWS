7413ed4163ac7c8e5769b58d78f74672d85818e3
Dr3xler <14806947+Dr3xler@users.noreply.github.com>
2021-09-24 22:48:56 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,12 +1,4 @@
 cd "C:/FWS/Downloads/"
-echo "choose what you want to install"
-echo "1: Firefox"
-echo "2: Sync"
-echo "3: BitDefender"
-echo "4: all"
-
-Rem input check what number with switch case, then install with start file.exe
-
 
 Rem install winget to handle the installation process
 Powershell.exe -executionpolicy remotesigned -File "install.ps1"
